4005d6c9bfdf4cc54d28e92285395ff6d8d988d9
souryana <souryanahachem@hotmail.com>
2023-05-05 23:34:49 +0200

    Parallelized code

--- Pseudonymisation.py
+++ Pseudonymisation.py
@@ -5,6 +5,7 @@ import base64
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes, kdf
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import concurrent.futures
 #needs 5mins to finish
 #Richest dataset
 df = pd.read_csv("500 richest people 2021.csv",sep=";")
@@ -48,47 +49,86 @@ def decrypt_id(id):
     return int(decrypted_data.decode())
 
 # Define a function to encrypt data
-def encrypt_name(key, name):
+def encrypt_name(id, name):
+    salt = str(id).encode()
+    kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(secret_key))
     f = Fernet(key)
     encrypted_data = f.encrypt(name.encode())
     return encrypted_data.decode()
 
 # Define a function to decrypt data
-def decrypt_name(key, name):
+def decrypt_name(id, name):
+    salt = str(id).encode()
+    kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(secret_key))
     f = Fernet(key)
     decrypted_data = f.decrypt(name.encode())
     return decrypted_data.decode()
 
 print("Encrypting...")
 
-for i, row in df.iterrows():
-    salt = str(row[0]).encode()
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(secret_key))
-    row[2] = encrypt_name(key,row[2])
-    row[0] = encrypt_id(row[0])
-    df.iloc[i] = row
+# for i, row in df.iterrows():
+#     salt = str(row[0]).encode()
+#     kdf = PBKDF2HMAC(
+#         algorithm=hashes.SHA256(),
+#         length=32,
+#         salt=salt,
+#         iterations=100000,
+#     )
+#     key = base64.urlsafe_b64encode(kdf.derive(secret_key))
+#     row[2] = encrypt_name(key,row[2])
+#     row[0] = encrypt_id(row[0])
+#     df.iloc[i] = row
+def encrypt_row(row):
+    new_row = list(row[:])
+    new_row[2] = encrypt_name(row[0], row[2])
+    new_row[0] = encrypt_id(row[0])
+    return new_row
 
-df.to_csv("500 richest people 2021 encrypted.csv", index=False)
+# df[2] = df.apply(lambda row: encrypt_name(row[0], row[2]), axis=1)
+# df[0] = df[0].apply(encrypt_id)
+# parralelize
+with concurrent.futures.ThreadPoolExecutor() as executor: #iterate with multiple threads
+    results = list(executor.map(encrypt_row, df.itertuples(index=False)))
+
+df = pd.DataFrame(results, columns=df.columns)
+df.to_csv("500 richest people 2021 encrypted2.csv", index=False)
 
 print("Decrypting...")
 
-for i, row in df.iterrows():
-    row[0] = decrypt_id(row[0])
-    salt = str(row[0]).encode()
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(secret_key))
-    row[2] = decrypt_name(key,row[2])
-    df.iloc[i] = row
+# for i, row in df.iterrows():
+#     row[0] = decrypt_id(row[0])
+#     salt = str(row[0]).encode()
+#     kdf = PBKDF2HMAC(
+#         algorithm=hashes.SHA256(),
+#         length=32,
+#         salt=salt,
+#         iterations=100000,
+#     )
+#     key = base64.urlsafe_b64encode(kdf.derive(secret_key))
+#     row[2] = decrypt_name(key,row[2])
+#     df.iloc[i] = row
 
-df.to_csv("500 richest people 2021 decrypted.csv", index=False)
+# df[0] = df[0].apply(decrypt_id)
+# df[2] = df.apply(lambda row: decrypt_name(row[0], row[2]), axis=1)
+def decrypt_row(row):
+    new_row = list(row[:])
+    new_row[0] = decrypt_id(row[0])
+    new_row[2] = decrypt_name(new_row[0], row[2])
+    return new_row
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = list(executor.map(decrypt_row, df.itertuples(index=False)))
+df = pd.DataFrame(results, columns=df.columns)
+
+df.to_csv("500 richest people 2021 decrypted2.csv", index=False)
--- Pseudonymisation_2.py
+++ Pseudonymisation_2.py
@@ -5,7 +5,9 @@ import base64
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes, kdf
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-#needs 5mins to finish
+import concurrent.futures
+
+#needs 2hrs to finish
 #age dataset
 df = pd.read_csv("AgeDataset-V3.csv",sep=",")
 df = df.drop(df.columns[0], axis=1)
@@ -35,47 +37,85 @@ def decrypt_id(id):
     return decrypted_data.decode()
 
 # Define a function to encrypt data
-def encrypt_name(key, name):
+def encrypt_name(id, name):
+    salt = id.encode()
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(secret_key))
     f = Fernet(key)
     encrypted_data = f.encrypt(name.encode())
     return encrypted_data.decode()
 
 # Define a function to decrypt data
-def decrypt_name(key, name):
+def decrypt_name(id, name):
+    salt = id.encode()
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(secret_key))
     f = Fernet(key)
     decrypted_data = f.decrypt(name.encode())
     return decrypted_data.decode()
 
 print("Encrypting...")
 
-for i, row in df.iterrows():
-    salt = row[0].encode()
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(secret_key))
-    row[1] = encrypt_name(key,row[1])
-    row[0] = encrypt_id(row[0])
-    df.iloc[i] = row
+# for i, row in df.iterrows():
+#     salt = row[0].encode()
+#     kdf = PBKDF2HMAC(
+#         algorithm=hashes.SHA256(),
+#         length=32,
+#         salt=salt,
+#         iterations=100000,
+#     )
+#     key = base64.urlsafe_b64encode(kdf.derive(secret_key))
+#     row[1] = encrypt_name(key,row[1])
+#     row[0] = encrypt_id(row[0])
+#     df.iloc[i] = row
 
-df.to_csv("AgeDataset-V3-encrypted.csv", index=False)
+def encrypt_row(row):
+    new_row = list(row[:])
+    new_row[1] = encrypt_name(row[0], row[1])
+    new_row[0] = encrypt_id(row[0])
+    return new_row
+
+# parralelize
+with concurrent.futures.ThreadPoolExecutor() as executor: #iterate with multiple threads
+    results = list(executor.map(encrypt_row, df.itertuples(index=False)))
+
+df = pd.DataFrame(results, columns=df.columns)
+
+df.to_csv("AgeDataset-V3-encrypted2.csv", index=False)
 
 print("Decrypting...")
 
-for i, row in df.iterrows():
-    row[0] = decrypt_id(row[0])
-    salt = row[0].encode()
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(secret_key))
-    row[1] = decrypt_name(key,row[1])
-    df.iloc[i] = row
+# for i, row in df.iterrows():
+#     row[0] = decrypt_id(row[0])
+#     salt = row[0].encode()
+#     kdf = PBKDF2HMAC(
+#         algorithm=hashes.SHA256(),
+#         length=32,
+#         salt=salt,
+#         iterations=100000,
+#     )
+#     key = base64.urlsafe_b64encode(kdf.derive(secret_key))
+#     row[1] = decrypt_name(key,row[1])
+#     df.iloc[i] = row
+def decrypt_row(row):
+    new_row = list(row[:])
+    new_row[0] = decrypt_id(row[0])
+    new_row[1] = decrypt_name(new_row[0], row[1])
+    return new_row
 
-df.to_csv("AgeDataset-V3-decrypted.csv", index=False)
+# parralelize
+with concurrent.futures.ThreadPoolExecutor() as executor: #iterate with multiple threads
+    results = list(executor.map(decrypt_row, df.itertuples(index=False)))
+
+df = pd.DataFrame(results, columns=df.columns)
+df.to_csv("AgeDataset-V3-decrypted2.csv", index=False)
